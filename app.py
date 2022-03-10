b2c1fef422ab9a2ccedacaa3d8d4c8d617d78f22
Ryan Serpico <Ryguy360@gmail.com>
2022-03-10 12:08:35 -0600

    Added scraper to grab San Antonio gas prices

---
+++ app.py
@@ -0,0 +1,31 @@
+from bs4 import BeautifulSoup
+# import requests
+from urllib.request import Request, urlopen
+import csv
+
+url = 'https://gasprices.aaa.com/?state=TX'
+
+def priceScraper(url):
+    req = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
+    page = urlopen(req).read()
+    soup = BeautifulSoup(page, 'html.parser')
+    cities = soup.find_all('h3')
+    # print(cities)
+    index = 0
+    for h3 in cities:
+        if h3.text == 'San Antonio':
+            # print('Found San Antonio! Its index is {}'.format(index))
+            break
+        else:
+            # print('Nope')
+            index += 1  
+    print('------------------------------------------------------')
+    sanAntonioRow = cities[index]
+    print(sanAntonioRow.text)
+    priceRow = sanAntonioRow.find_next_sibling().find_all('tr')[1]
+    regularGasPrice = '$' + str(round(float(priceRow.find_all('td')[1].text[1:]), 2))
+    dieselGasPrice = '$' + str(round(float(priceRow.find_all('td')[4].text[1:]), 2))
+    print('Regular: {}'.format(regularGasPrice))
+    print('Diesel: {}'.format(dieselGasPrice))
+
+priceScraper(url)
