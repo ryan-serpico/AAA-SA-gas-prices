2dac49d5bd7f12f816e41d25a9b4123c27f8f17f
Ryan Serpico <Ryguy360@gmail.com>
2022-03-10 15:30:31 -0600

    Created dataframe for table

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from bs4 import BeautifulSoup
-# import requests
 from urllib.request import Request, urlopen
+import pandas as pd
 import csv
 
 url = 'https://gasprices.aaa.com/?state=TX'
@@ -25,7 +25,13 @@ def priceScraper(url):
     priceRow = sanAntonioRow.find_next_sibling().find_all('tr')[1]
     regularGasPrice = '$' + str(round(float(priceRow.find_all('td')[1].text[1:]), 2))
     dieselGasPrice = '$' + str(round(float(priceRow.find_all('td')[4].text[1:]), 2))
+    prices = [regularGasPrice, dieselGasPrice]
     print('Regular: {}'.format(regularGasPrice))
     print('Diesel: {}'.format(dieselGasPrice))
+
+    df = pd.DataFrame()
+    df[''] = ['Regular', 'Diesel']
+    df['Price'] = prices
+    print(df)
 
 priceScraper(url)
